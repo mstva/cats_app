c040aee17be2aba710d66be17b3c30ae2e059be7
mstva <mustafa.abdulluh@gmail.com>
2022-03-29 15:41:03 +0200

    added and setup Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as chakra from '@chakra-ui/react'
+
+
+export const App = () => {
+  return (
+      <chakra.Center>
+          <chakra.Text>Hello</chakra.Text>
+      </chakra.Center>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react'
+import {App} from "./App";
 
 ReactDOM.render(
-  <div>
-    Hello
-  </div>,
+    <ChakraProvider>
+        <App />
+    </ChakraProvider>,
   document.getElementById('root')
 );
